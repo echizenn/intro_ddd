6b0e85540f0293d958c5ed263b15c86b0485b7ad
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-01-08 22:57:04 +0900

    feat: finish chap2

---
+++ chap2/ddd2_5_2.py
@@ -0,0 +1,42 @@
+"""
+2.5.2節のコードの説明
+"""
+
+# リスト2.36
+def list2_36():
+    """
+    存在してはいけない値
+
+    Args: None
+
+    Returns: None
+
+    Note:
+        「ユーザ名は3文字以上」というルールが存在している
+    """
+    user_name: str = "me"
+
+# リスト2.37
+def list2_37(user_name: str):
+    if len(user_name) >= 3:
+        # 正常な値なので処理を継続する
+        pass
+    else:
+        raise ValueError("異常な値です")
+
+# リスト2.38
+class UserName:
+    """
+    ユーザ名を表す値オブジェクト
+
+    Attributes:
+        value (str): ユーザ名の値
+    
+    Note:
+        ガード節によって不正な値の存在を考える必要がなくなる
+    """
+    def __init__(self, value: str):
+        # ガード節
+        if len(value) < 3: raise ValueError("ユーザ名は3文字以上です。")
+
+        self.value = value
