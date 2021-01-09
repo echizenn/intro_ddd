bbfd5f4e994ace3ba22ab323129e3abcb2ad57cf
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-01-09 16:46:42 +0900

    feat: finish chap3

---
+++ chap3/ddd3_5.py
@@ -0,0 +1,34 @@
+"""
+3.5節のコードの説明
+"""
+
+# リスト3.8
+class User:
+    """
+    無口なコード
+    ここでの目的上説明用のdocsはこれ以上書きません
+
+    Note:
+        ユーザ名が文字列ということしかわからない
+    """
+    def __init__(self, name: str):
+        self.name: str = name
+
+# リスト3.9
+class UserName:
+    """
+    饒舌なコード
+
+    Attributes:
+        _value (str): ユーザ名
+
+    Raises:
+        ValueError: ユーザ名が3文字未満のとき
+
+    Note:
+        ユーザ名が3文字以上でなければ動作しないことはコードだけ見てもわかる
+    """
+    def __init__(self, value: str):
+        if len(value) < 3: raise ValueError("ユーザ名は3文字以上です")
+
+        self._value = value
