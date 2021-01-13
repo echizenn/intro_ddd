7627d2a9e651d20163b15f3e2072ccd92203be90
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-01-13 15:15:18 +0900

    feat: finish chap5

---
+++ chap5/ddd5_8_1.py
@@ -0,0 +1,70 @@
+"""
+5.8.1節のコードの説明
+"""
+from abc import ABCMeta, abstractmethod
+
+class IUserRepository(metaclass=ABCMeta):
+    """
+    Userクラスのリポジトリインターフェース
+    """
+
+    # リスト5.22
+    @abstractmethod
+    def save(self, user: User):
+        """
+        永続化を行うふるまい
+        名前はstoreとかでも良い
+
+        Args:
+            user (User): 保存したいUserインスタンス
+
+        Returns: None
+        """
+        pass
+
+    # リスト5.23
+    @abstractmethod
+    def update_name(self, id: UserId, name: UserName):
+        """
+        ユーザ名を更新する
+        更新項目を引き渡す更新処理(悪い例)
+        永続化を行うオブジェクトを引数にとっていないのがおかしい
+
+        Args:
+            id (UserId): 更新したいユーザのid
+            name: (UserName): 更新したいユーザ名
+        
+        Returns: None
+        """
+        pass
+
+    # リスト5.24
+    @abstractmethod
+    def update_email(self, id: UserId, mail: Email):
+        """
+        メールアドレスを更新する
+        更新項目を引き渡す更新処理(悪い例)
+        永続化を行うオブジェクトを引数にとっていないのがおかしい
+        このようにメソッドが乱立してしまう
+
+        Args:
+            id (UserId): 更新したいユーザのid
+            name: (Email): 更新したいメールアドレス
+        
+        Returns: None
+        """
+        pass
+
+    # リスト5.25
+    @abstractmethod
+    def delete(self, name: User):
+        """
+        ユーザの削除
+        破棄を行うふるまいを定義したリポジトリ
+
+        Args:
+            user (User): 削除したいユーザ
+        
+        Returns: None
+        """
+        pass
---
+++ chap5/ddd5_8_2.py
@@ -0,0 +1,47 @@
+"""
+5.8.2節のコードの説明
+"""
+from abc import ABCMeta, abstractmethod
+from typing import List
+
+class IUserRepository(metaclass=ABCMeta):
+    """
+    Userクラスのリポジトリインターフェース
+    """
+
+    # リスト5.26
+    @abstractmethod
+    def find(self, id: UserId) -> User:
+        """
+        ユーザidによるインスタンスの復元
+        識別子によって検索されるメソッド
+
+        Args:
+            id (UserId): 復元したいユーザid
+        
+        Returns:
+            User: 復元されたUserインスタンス
+        """
+        pass
+
+    # リスト5.27
+    @abstractmethod
+    def find_all(self) -> List[User]:
+        """
+        すべてのオブジェクトを再構築するメソッド
+
+        Args: None
+        
+        Returns:
+            List[User]: すべてのUserインスタンス
+        """
+        pass
+
+    # リスト5.28
+    @abstractmethod
+    def find_by_user_name(self, name: UserName) -> User:
+        """
+        探索に適したメソッド
+        Pythonではオーバーロードがサポートされていないので、命名によりバリエーションを増やす
+        """
+        pass
