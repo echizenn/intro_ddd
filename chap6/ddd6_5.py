659e621cfe97db7f124f19821e0a26ae22cc0d78
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-02-14 23:02:25 +0900

    feat: finish 6.5

---
+++ chap6/ddd6_5.py
@@ -0,0 +1,93 @@
+"""
+6.5節のコードの説明
+"""
+from abc import ABCMeta, abstractmethod
+from dataclasses import dataclass
+
+# リスト6.35
+class IUserRegisterService(metaclass=ABCMeta):
+    """
+    ユーザ登録処理のインターフェース
+
+    Attributes: None
+    """
+    @abstractmethod
+    def handle(self, command: UserRegisterCommand):
+        """
+        実行
+
+        Args:
+            command (UserRegisterCommand): 登録したいユーザ
+        
+        Returns: None
+        """
+        pass
+
+
+# リスト6.36
+@dataclass
+class Client:
+    """
+    クライアントはインターフェースを利用する
+
+    Attributes:
+        _user_repository_service (IUserRegisterService): ユーザーレポジトリサービス
+    """
+    _user_repository_service: IUserRegisterService
+
+    def register(self, name: str):
+        """
+        ユーザ登録処理
+
+        Args:
+            name (str): ユーザ名
+        
+        Returns: None
+
+        Raises:
+            ValueError: 同一ユーザが存在している場合
+        """
+        command = UserRegisterCommand(name)
+        self._user_repository_service.handle(command)
+
+
+# リスト6.37
+class MockUserRegisterService(IUserRegisterService):
+    """
+    ユーザ登録処理のインターフェース
+
+    Attributes: None
+    """
+    @abstractmethod
+    def handle(self, command: UserRegisterCommand):
+        """
+        実行
+
+        Args:
+            command (UserRegisterCommand): 登録したいユーザ
+        
+        Returns: None
+        """
+        pass
+
+# リスト6.38
+class ExceptionUserRegisterService(IUserRegisterService):
+    """
+    例外を送出させるモックオブジェクト
+
+    Attributes: None
+    """
+    @abstractmethod
+    def handle(self, command: UserRegisterCommand):
+        """
+        実行
+
+        Args:
+            command (UserRegisterCommand): 登録したいユーザ
+        
+        Returns: None
+
+        Raises:
+            ComplexException: 例外
+        """
+        raise ComplexExeption()
