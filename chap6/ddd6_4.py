7cf929059b71613fe0cd95ca8279812af1f97fc4
TomohiroFukui <echizenn_brave8150@icloud.com>
2021-02-14 22:46:00 +0900

    feat: finish 6.4

---
+++ chap6/ddd6_4.py
@@ -0,0 +1,59 @@
+"""
+6.4節のコードの説明
+"""
+from dataclasses import dataclass
+
+# リスト6.30
+@dataclass
+class LowCohesion:
+    """
+    凝集度が低いクラス
+
+    Attributes:
+        _value1 (int): フィールド
+        _value2 (int): フィールド
+        _value3 (int): フィールド
+        _value4 (int): フィールド
+    """
+    _value1: int
+    _value2: int
+    _value3: int
+    _value4: int
+
+    def method_a(self) -> int:
+        return self._value1 + self._value2
+    
+    def method_b(self) -> int:
+        return self._value3 + self._value4
+
+# リスト6.31
+@dataclass
+class HighCohesionA:
+    """
+    分離することで凝集度を高めたクラス
+
+    Attributes:
+        _value1 (int): フィールド
+        _value2 (int): フィールド
+    """
+    _value1: int
+    _value2: int
+
+    def method_a(self) -> int:
+        return self._value1 + self._value2
+
+
+@dataclass
+class HighCohesionB:
+    """
+    凝集度が低いクラス
+
+    Attributes:
+        _value3 (int): フィールド
+        _value4 (int): フィールド
+    """
+    _value3: int
+    _value4: int
+
+    def method_b(self) -> int:
+        return self._value3 + self._value4
