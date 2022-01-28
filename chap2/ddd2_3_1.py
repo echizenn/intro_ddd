28b8f09de213a9dbdc08a67c6cc74f453591016d
TomohiroFukui <echizenn_brave8150@icloud.com>
2022-01-28 14:42:13 +0900

    fix: valueが適切が確認

--- chap2/ddd2_3_1.py
+++ chap2/ddd2_3_1.py
@@ -4,6 +4,36 @@
 import dataclasses
 from typing import Final # 再代入不可能という型ヒント
 
+# リスト2.22
+@dataclasses.dataclass(frozen=True)
+class FirstName:
+    """
+    名を表す値オブジェクト
+
+    Attributes:
+        _value (str): 名前
+    """
+    _value: Final[str]
+
+    def __init__(self, value: str) -> None:
+        if len(value) < 1: raise ArgumentException("1文字以上である必要があります。", str(value))
+        self._value = value
+
+# リスト2.23
+@dataclasses.dataclass(frozen=True)
+class LastName:
+    """
+    姓を表す値オブジェクト
+
+    Attributes:
+        _value (str): 姓
+    """
+    _value: Final[str]
+
+    def __init__(self, value: str) -> None:
+        if len(value) < 1: raise ArgumentException("1文字以上である必要があります。", str(value))
+        self._value = value
+        
 # リスト2.21
 @dataclasses.dataclass(frozen=True)
 class FullName:
@@ -20,25 +50,3 @@ class FullName:
     """
     _first_name: Final[FirstName]
     _last_name: Final[LastName]
-
-# リスト2.22
-@dataclasses.dataclass(frozen=True)
-class FirstName:
-    """
-    名を表す値オブジェクト
-
-    Attributes:
-        _value (str): 名前
-    """
-    _value: Final[str]
-
-# リスト2.23
-@dataclasses.dataclass(frozen=True)
-class LastName:
-    """
-    姓を表す値オブジェクト
-
-    Attributes:
-        _value (str): 姓
-    """
-    _value: Final[str]
