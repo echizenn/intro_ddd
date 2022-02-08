15d1faad8a3ff76d1df9e0683d94030316c341a3
TomohiroFukui <echizenn_brave8150@icloud.com>
2022-02-08 17:17:14 +0900

    refactor: import周り修正

--- chap13/ddd13_1_1_1.py
+++ chap13/ddd13_1_1_1.py
@@ -1,11 +1,20 @@
 """
 13.1.1節のコードの説明
 """
+import dataclasses
+from typing import List
 
 # リスト13.1
+@dataclasses.dataclass
 class Circle:
     """
     条件にしたがっているかを評価するふるまい
     """
+    _owner: User
+    _members: List[User]
+
     def is_full(self) -> bool:
         return self.count_members() >= 30
+
+    def count_members(self) -> int:
+        return len(self._members) + 1
